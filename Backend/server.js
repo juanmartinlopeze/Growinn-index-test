dbf1ee842630a17034f799e4e1f9d8b914827352
Juan Esteban Ramirez <juanestebanrp123@gmail.com>
2025-03-25 14:34:01 -0500

    Configuración servery db

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const sequelize = require("./db");
-const Usuario = require("./models/usuarios");
+const Empresa = require("./models/empresa");
 
 const app = express();
 app.use(express.json());
@@ -10,31 +10,36 @@ app.use(cors());
 // Sincronizar la base de datos sin eliminar datos existentes
 sequelize.sync({ alter: true }).then(() => {
   console.log("✅ Base de datos sincronizada");
+
 });
 
-// ✅ Ruta para crear un usuario (con nombre y email)
-app.post("/usuarios", async (req, res) => {
-  try {
-    const { name, email } = req.body;
-    if (!name || !email) {
-      return res.status(400).json({ error: "Faltan datos requeridos" });
+app.post("/empresas", async (req, res) => {
+  try{
+    const {empleados, jerarquia1, jerarquia2, jerarquia3, jerarquia4, areas} = req.body;
+    if(!empleados || !jerarquia1 || !jerarquia2 || !jerarquia3 || !jerarquia4 || !areas){
+      return res.status(400).json({error: "Faltan datos requeridos"});
     }
-    const usuario = await Usuario.create({ name, email });
-    res.status(201).json(usuario);
-  } catch (error) {
-    res.status(500).json({ error: "Error al crear usuario" });
+    const empresa = await Empresa.create({empleados, jerarquia1, jerarquia2, jerarquia3, jerarquia4, areas});
+    res.status(201).json(empresa);
+
+  }
+  catch(error){
+    res.status(500).json({error: "Error al crear empresa"});
+
   }
 });
 
-// ✅ Ruta para obtener todos los usuarios
-app.get("/usuarios", async (req, res) => {
-  try {
-    const usuarios = await Usuario.findAll();
-    res.status(200).json(usuarios);
-  } catch (error) {
-    res.status(500).json({ error: "Error al obtener usuarios" });
+app.get("/empresas", async (req, res) => {
+  try{
+    const empresas = await Empresa.findAll();
+    res.status(200).json(empresas);
+  }
+  catch(error){
+    res.status(500).json({error: "Error al obtener empresas"});
+
   }
 });
+
 
 app.listen(3000, () => {
   console.log("🚀 Servidor corriendo en http://localhost:3000");
