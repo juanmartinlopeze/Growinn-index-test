a1492bd01b6515b9a0bfa740647eab25a66cfaa1
Martin Lopez <juanmartinlopez0823@gmail.com>
2025-04-27 21:15:54 -0500

    feat: implementar rediseño de los popupss
    - Modificado EditRoleModal.jsx y EditAreaForm.jsx para mejorar disposición de inputs
    - Añadido diseño responsivo y cambios de estilo en Table.css
    - Mejorada experiencia de usuario con botones de acción alineados y subcargos gestionables
    - Implementado cambio de color en íconos SVG al hacer hover
    
    See #33

--- Frontend/src/components/Table/EditAreaForm.jsx
+++ Frontend/src/components/Table/EditAreaForm.jsx
@@ -8,10 +8,16 @@ export default function EditAreaForm({ areaName, onChange, onSave, onCancel, onD
 				<div className='modal-content'>
 					<h3>Editar nombre del área</h3>
 					<input value={areaName} onChange={(e) => onChange(e.target.value)} placeholder='Nuevo nombre del área' />
-					<div className='modal-buttons'>
-						<Button variant='submit' className='submit-button' onClick={onSave} />
-						<Button variant='delete' className='delete-button' onClick={onDelete} />
-						<Button variant='cancel' className='cancel-button' onClick={onCancel} />
+					<div className='action-buttons'>
+						<Button type='button' variant='cancel' className='small-button' onClick={onCancel}>
+							Cancelar
+						</Button>
+						<Button type='button' variant='delete' className='small-button' onClick={onDelete}>
+							Eliminar
+						</Button>
+						<Button type='submit' variant='submit' className='small-button' onClick={onSave}>
+							Guardar
+						</Button>
 					</div>
 				</div>
 			</div>
